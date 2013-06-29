7d845f7340a25293274e46b0fc5a7207be353972
Eric L <elech@ilstu.edu>
2013-06-29 15:04:19 -0500

    Merged front n Back

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ db.once('open', function() {
 app.use(express.bodyParser());
 //needed for legacy support of DELETE http method
 app.use(express.methodOverride());
-app.use(express.static(_dirname + '../frontend/app'))
+app.use(express.static(_dirname + 'public'))
 
 //bootstrap your routes
 
